f706d7ff467d015500257b5eb5cad359bbf87c38
AlanSun <alansun2020@126.com>
2023-03-27 13:52:29 -0700

    Create Event frontend

--- App.js
+++ App.js
@@ -1,28 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,Button, Text, View, TextInput } from 'react-native';
-import React, {useState} from 'react';
-import MapView, {Callout, Marker} from 'react-native-maps';
+import { StyleSheet, Button, Text, View, SafeAreaView, TextInput, Modal, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import MapView, { Callout, Marker } from 'react-native-maps';
+// import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { BlurView } from 'expo-blur';
+
+export default function App() {
+
+  const [current, setCurrent] = useState('Home');   // navigation
+  const [poi, setPoi] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [location, setLocation] = useState({ latitude: 32.88123691352959, longitude: -117.23760781304348 });
+  const [eventForm, setEventForm] = useState({ title: '', description: '', category: '', location: '' });
+
+  function onPress(e) {
+    console.log("onPress ++++++++");
+    console.log(e.nativeEvent.coordinate);
+    // console.log(e.placeId, e.name, e.coordinate);  // undefined x3 
+    // 需要后端：(经度、纬度) --> 从已创建的events中找到最近的event(groups)。
+    // If there's are event(s) here around, show them as popup(s)
+    setPoi(e.nativeEvent);
+  }
 
 
- export default function App() {
-  
 
-
-    const [current, setCurrent] = useState('Home');
-    const [poi, setPoi] = useState(null);
-
-
-
-    function onPress(e) {
-      console.log("onPress ++++++++");
-      console.log(e.nativeEvent);
-      console.log(e.placeId, e.name, e.coordinate); // undefined x3
-      const newPoi = e.nativeEvent;
-      setPoi(newPoi);
-    }
-
-    const HomeScreen =  (
-      <View style={styles.container}>
+  const HomeScreen = (
+    <View style={styles.container}>
       <MapView style={styles.map}
         //template for region I guess
         initialRegion={{
@@ -46,71 +49,113 @@ import MapView, {Callout, Marker} from 'react-native-maps';
 
 
 
-     
-        <View style={styles.buttonContainer}>
-         <Button 
-          title="select a place"
+      {/* Create Event button */}
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Create Event"
           color="black"
-          onPress={() => setCurrent(EventCreatorScreen)}
-    
-          ></Button>
-        </View>
-      </View>
-    );
+          // onPress={() => setCurrent(EventCreatorScreen)}
+          onPress={() => setModalVisible(true)}
 
-
-    
-
-    //change map region data
-    // function onRegionChange(region) {
-    //   setState({ region });
-    // }
-    
-    //update new region
-    // function render() {
-    //   return (
-    //     <MapView
-    //       region={this.state.region}
-    //       onRegionChange={this.onRegionChange}
-    //       onPoiClick={this.onPoiClick}>
-    //       {state.poi && (
-    //         <Marker coordinate={this.state.poi.coordinate}>
-    //           <Callout>
-    //             <View>
-    //               <Text>Place Id: {this.state.poi.placeId}</Text>
-    //               <Text>Name: {this.state.poi.name}</Text>
-    //             </View>
-    //           </Callout>
-    //         </Marker>
-    //       )}
-    //     </MapView>
-    //   );
-    // }
-
-
-    //screen for event post
-    const EventCreatorScreen = (
-      <View style={styles.eventPost}>
-        
-        <TextInput style={styles.input} placeholder="Selected location" />
-
-        
-
+        ></Button>
       </View>
 
-    );
+      <Modal visible={modalVisible} transparent={true} animationType="slide">
+        <BlurView style={{ flex: 1 }} intensity={60}>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Text style={styles.title}> Create your event </Text>
 
-    //return render;
-    
-    return current === 'Home' ? HomeScreen: EventCreatorScreen;
+            <Text style={styles.label}> Title </Text>
+            <TextInput style={styles.input} placeholder="Event name"
+              value={eventForm.title}
+              onChangeText={txt => setEventForm({ ...eventForm, title: txt })} />
 
- 
-  
+            <Text style={styles.labelSmall}> Description (optional) </Text>
+            <TextInput
+              style={styles.inputSmall}
+              placeholder="What this event is about/ how to find it"
+              value={eventForm.description}
+              onChangeText={txt => { setEventForm({ ...eventForm, description: txt }); console.log(eventForm) }} />
+
+            <Text style={styles.label}> Category </Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 30 }}>
+
+              <TouchableOpacity
+                style={eventForm.category == 'Food' ? { ...styles.button, backgroundColor: '#FFD580' } : styles.button}
+                onPress={e => setEventForm({ ...eventForm, category: 'Food' })}>
+                <Text style={styles.buttonText}>Food</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={eventForm.category == 'Study' ? { ...styles.button, backgroundColor: '#FFD580' } : styles.button}
+                onPress={e => setEventForm({ ...eventForm, category: 'Study' })}>
+                <Text style={styles.buttonText}>Study</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={eventForm.category == 'Hangout' ? { ...styles.button, backgroundColor: '#FFD580' } : styles.button}
+                onPress={e => setEventForm({ ...eventForm, category: 'Hangout' })}>
+                <Text style={styles.buttonText}>Hangout</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={eventForm.category == 'Other' ? { ...styles.button, backgroundColor: '#FFD580' } : styles.button}
+                onPress={e => setEventForm({ ...eventForm, category: 'Other' })}>
+                <Text style={styles.buttonText}>Other</Text>
+              </TouchableOpacity>
+
+            </View>
+
+            <Text style={styles.label}> Location </Text>
+
+            <TextInput style={styles.inputSmall} placeholder="Search for location" />
+
+
+            <MapView
+              style={styles.mapSmall}
+              initialRegion={{ latitude: location.latitude, longitude: location.longitude, latitudeDelta: 0.0007, longitudeDelta: 0.0007, }}
+            >
+              <Marker coordinate={location} />
+            </MapView>
+
+
+
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+
+              <TouchableOpacity style={styles.controlButton} onPress={() => setModalVisible(false)}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Cancel</Text>
+              </TouchableOpacity>
+
+
+            </View>
+          </SafeAreaView>
+        </BlurView>
+      </Modal>
+    </View>
+
+
+
+
+  );
+
+  return current === 'Home' ? HomeScreen : EventCreatorScreen;
+
+
+
 }
 
 
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 37,
+    fontWeight: 'bold',
+    marginTop: 40,
+    marginBottom: 30,
+    textAlign: 'left',
+    marginLeft: 30,
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
@@ -119,36 +164,130 @@ const styles = StyleSheet.create({
   },
   eventPost: {
     flex: 1,
-    backgroundColor:'#F5F5F5',
+    backgroundColor: '#F5F5F5',
+  },
+  label: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    textAlign: 'left',
+    marginLeft: 30,
+
+    fontSize: 18,
+  },
+  labelSmall: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    textAlign: 'left',
+    marginLeft: 30,
+
+    fontSize: 14,
   },
   input: {
-    width: 300,
+    // width: 300,
     height: 40,
-    backgroundColor: '#fff',
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    borderColor: '#5470FF',
-    borderWidth: 1,
-    borderRadius: 15, 
-    fontSize: 16,
+    marginHorizontal: 30,
+    marginTop: 5,
+    paddingHorizontal: 20,
+    // borderWidth: 1,
+    borderRadius: 30,
+    alignSelf: 'stretch',
 
-    width: 233,
-    height: 36,
-    left: 76,
-    top: 127,
-},
+    backgroundColor: '#fff',
+    borderColor: 'green',
+
+
+    fontSize: 18,
+  },
+  inputSmall: {
+    height: 35,
+    marginHorizontal: 30,
+    marginTop: 5,
+    paddingHorizontal: 20,
+    // borderWidth: 1,
+    borderRadius: 30,
+    alignSelf: 'stretch',
+
+    backgroundColor: '#fff',
+    borderColor: 'green',
+
+    fontSize: 15,
+  },
+  button: {
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    margin: 10,
+
+    borderRadius: 15,
+
+  },
+  buttonText: {
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  mapSmall: {
+    height: 200,
+    marginVertical: 10,
+    marginHorizontal: 30,
+  },
+
+  controlButton: {
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 15,
+
+  },
+  // Create Event button
   buttonContainer: {
-    backgroundColor: '#D9D9D9',
+    backgroundColor: '#FFFFFF',
+    padding: 10,
     width: 235,
     height: 77,
     position: 'absolute',
     top: 721,
     left: 78,
-    fontWeight: 400,
+    borderRadius: 15,
 
   },
   map: {
     width: '100%',
     height: '100%',
   }
-})
+});
+
+const searchBarStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textInputContainer: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+
+  },
+  textInput: {
+    marginLeft: 0,
+    marginRight: 0,
+    height: 38,
+    color: '#5d5d5d',
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 20,
+    padding: 10,
+  },
+  listView: {
+    backgroundColor: 'white',
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 5,
+    elevation: 1,
+    zIndex: 1,
+  },
+  poweredContainer: {
+    height: 0,
+    opacity: 0,
+  },
+});
